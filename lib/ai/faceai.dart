2c2ebf22510bbca465b338d53f1b5b1347669589
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-05-15 15:59:13 +0200

    :remove add assets

---
+++ lib/ai/faceai.dart
@@ -0,0 +1,66 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:moodai/painters/face_detector_painter.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import '../camera/camera_view.dart';
+
+
+class FaceDetectorView extends StatefulWidget {
+  @override
+  State<FaceDetectorView> createState() => _FaceDetectorViewState();
+}
+
+class _FaceDetectorViewState extends State<FaceDetectorView> {
+  final FaceDetector _faceDetector = FaceDetector(
+    options: FaceDetectorOptions(
+      enableContours: true,
+      enableClassification: true,
+    ),
+  );
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    _faceDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraView(
+      customPaint: _customPaint,
+      text: _text,
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
+      initialDirection: CameraLensDirection.front,
+    );
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final faces = await _faceDetector.processImage(inputImage);
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = FaceDetectorPainter(
+          faces,
+          inputImage.inputImageData!.size,
+          inputImage.inputImageData!.imageRotation);
+      _customPaint = CustomPaint(painter: painter);
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'ai/faceai.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: FaceDetectorView());
+  }
+}
